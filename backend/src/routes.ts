0efbc6f7a5aa6de771a6eacca8eb9b3ce38b1876
Bernardo <bernardowerty@outlook.com>
2026-01-17 17:01:13 -0300

    criação do schema e validação middleware na criação users

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,11 +1,13 @@
 import {Router, Request, Response} from 'express';
 import { CreateUserController } from './controllers/user/CreateUserController';
+import { validadeSchema } from './middlewares/validateSchema';
+import { createUserSchema } from './schemas/userSchema';
 
 
 const router = Router();
 
 
-router.post("/users", new CreateUserController().handle);
+router.post("/users", validadeSchema(createUserSchema), new CreateUserController().handle);
 
 // arquiteturea em camadas routes-cotroller-service, arquitetura em camadas
 
